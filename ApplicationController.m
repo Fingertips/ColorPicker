eec5852ee62277e4c7366fd033d9bcca41ffe6a7
Eloy Duran <eloy@Eloy-Durans-Mac-Pro.local>
2010-03-26 10:47:13 +0100

    Show the opacity control

--- ApplicationController.m
+++ ApplicationController.m
@@ -4,11 +4,13 @@
 @implementation ApplicationController
 - (void)awakeFromNib
 {
+  [NSColorPanel setPickerMask: NSColorPanelAllModesMask];
   NSColorPanel *panel = [NSColorPanel sharedColorPanel];
   [panel setDelegate: self];
-  [panel setStyleMask:NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask];
-  [panel setFloatingPanel:YES];
-  [panel makeKeyAndOrderFront:self];
+  [panel setStyleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask];
+  [panel setFloatingPanel: YES];
+  [panel setShowsAlpha: YES];
+  [panel makeKeyAndOrderFront: self];
 }
 
 - (void)windowWillClose:(NSNotification *)aNotification
