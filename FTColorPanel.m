f9aac1269f9141d4e4d265227336e77682692bf0
Eloy Duran <eloy.de.enige@gmail.com>
2010-03-30 12:46:57 +0200

    Small cleanup

--- FTColorPanel.m
+++ FTColorPanel.m
@@ -11,7 +11,7 @@
   if ([super init]) {
     _colorMode = HEX_COLOR_MODE;
     
-    [self setStyleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask];
+    [self setStyleMask: NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask];
     [self setHidesOnDeactivate: NO];
     [self setFloatingPanel: NO];
     [self setShowsAlpha: YES];
@@ -22,25 +22,24 @@
     // a bit smaller to make place for the new divider and text field and move
     // it up by the same offset to locate it in the original position.
     id *contentBox = [[[self contentView] subviews] objectAtIndex: CONTENT_BOX_INDEX];
-    NSRect contentBoxFrame = [contentBox frame];
-    contentBoxFrame.origin.y += CONTENT_BOX_OFFSET;
+    NSRect contentBoxFrame       = [contentBox frame];
+    contentBoxFrame.origin.y    += CONTENT_BOX_OFFSET;
     contentBoxFrame.size.height -= CONTENT_BOX_OFFSET;
     [contentBox setFrame: contentBoxFrame];
     
     // Add a new divider under the opacity slider
-    NSBox *divider = [[[self contentView] subviews] objectAtIndex: DIVIDER_INDEX];
-    // NSRect newDividerFrame = NSOffsetRect([divider frame], 0, 26);
+    NSBox *divider         = [[[self contentView] subviews] objectAtIndex: DIVIDER_INDEX];
     NSRect newDividerFrame = NSMakeRect(0, contentBoxFrame.origin.y - (SPACING - 1), totalWidth, 1);
-    NSBox *newDivider = [[NSBox alloc] initWithFrame: newDividerFrame];
+    NSBox *newDivider      = [[NSBox alloc] initWithFrame: newDividerFrame];
     [newDivider setBoxType: NSBoxSeparator];
     [newDivider setAutoresizingMask: NSViewWidthSizable];
     [[self contentView] addSubview: newDivider];
     
     // Add the new text field underneath the new divider and the existing divider above
-    float fontSize = [NSFont smallSystemFontSize];
-    float colorCodeFieldY = newDividerFrame.origin.y - (fontSize + SPACING);
-    colorCodeField = [[NSTextField alloc] initWithFrame: NSMakeRect(SPACING, colorCodeFieldY, totalWidth - (2 * SPACING), fontSize + 2)];
-    [colorCodeField setAutoresizingMask: NSViewWidthSizable|NSViewMaxYMargin];
+    float fontSize             = [NSFont smallSystemFontSize];
+    float colorCodeFieldY      = newDividerFrame.origin.y - (fontSize + SPACING);
+    NSRect colorCodeFieldFrame = NSMakeRect(SPACING, colorCodeFieldY, totalWidth - (2 * SPACING), fontSize + 2);
+    colorCodeField = [[NSTextField alloc] initWithFrame: colorCodeFieldFrame];
     // set the text properties
     [[colorCodeField cell] setFont: [NSFont systemFontOfSize: fontSize]];
     [colorCodeField setAlignment: NSCenterTextAlignment];
@@ -49,6 +48,7 @@
     [colorCodeField setDrawsBackground: NO];
     [colorCodeField setEditable: NO];
     [colorCodeField setSelectable: NO];
+    [colorCodeField setAutoresizingMask: NSViewWidthSizable | NSViewMaxYMargin];
     [[self contentView] addSubview: colorCodeField];
     
     [self updateStringRepresentationOfColor];
@@ -77,11 +77,11 @@
   
   switch (_colorMode) {
     case HEX_COLOR_MODE:             return [color toHexString];
-    case RGB_COLOR_MODE:             return [color toRGBString: shortVersion];
-    case RGBA_COLOR_MODE:            return [color toRGBAString: shortVersion];
-    case HSL_COLOR_MODE:             return [color toHSLString: shortVersion];
-    case HSLA_COLOR_MODE:            return [color toHSLAString: shortVersion];
-    case OBJC_NSCOLOR_COLOR_MODE:    return [color toObjcNSColor: shortVersion];
+    case RGB_COLOR_MODE:             return [color toRGBString:      shortVersion];
+    case RGBA_COLOR_MODE:            return [color toRGBAString:     shortVersion];
+    case HSL_COLOR_MODE:             return [color toHSLString:      shortVersion];
+    case HSLA_COLOR_MODE:            return [color toHSLAString:     shortVersion];
+    case OBJC_NSCOLOR_COLOR_MODE:    return [color toObjcNSColor:    shortVersion];
     case MACRUBY_NSCOLOR_COLOR_MODE: return [color toMacRubyNSColor: shortVersion];
   }
 }
