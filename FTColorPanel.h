6207ca4f84b3748fd2434739667d55a237138998
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2012-06-25 16:11:56 +0200

    Use ordered labels in the "Copy as..." sub menu

--- FTColorPanel.h
+++ FTColorPanel.h
@@ -2,14 +2,14 @@
 #import "NSColorAdditions.h"
 
 #define HEX_COLOR_MODE 0
-#define RGB_COLOR_MODE 1
-#define RGBA_COLOR_MODE 2
-#define HSL_COLOR_MODE 3
-#define HSLA_COLOR_MODE 4
-#define OBJC_NSCOLOR_COLOR_MODE 5
-#define MACRUBY_NSCOLOR_COLOR_MODE 6
-#define OBJC_UICOLOR_COLOR_MODE 7
-#define HEX_WITHOUT_HASH_COLOR_MODE 8
+#define HEX_WITHOUT_HASH_COLOR_MODE 1
+#define RGB_COLOR_MODE 2
+#define RGBA_COLOR_MODE 3
+#define HSL_COLOR_MODE 4
+#define HSLA_COLOR_MODE 5
+#define OBJC_NSCOLOR_COLOR_MODE 6
+#define MACRUBY_NSCOLOR_COLOR_MODE 7
+#define OBJC_UICOLOR_COLOR_MODE 8
 
 @interface FTColorPanel : NSColorPanel {
   NSTextField *colorCodeField;
